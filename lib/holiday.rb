5a0ef0379356e3ee78da46fa5ce003f06cf2e2da
Scott Silverman <scottms724@gmail.com>
2018-05-14 16:15:59 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -28,6 +28,7 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   holiday_hash.each do |season, holiday|
     if season == winter 
       season.each do |holiday, supplies|
+        supplies << supply 
 
 end
 
