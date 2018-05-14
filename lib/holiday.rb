30032d0daad3c8550fa3bdad497da1979735cc1c
Scott Silverman <scottms724@gmail.com>
2018-05-14 18:38:00 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -57,7 +57,7 @@ def all_supplies_in_holidays(holiday_hash)
     season_array.collect! do |season|
     season.capitalize!
   end
-    puts "#{season_array.join(" ")}:"
+    puts "#{season_array.join(" ")}: #{supplies.join"
     
 end
 
